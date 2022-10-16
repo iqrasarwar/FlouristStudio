6744623de5143c40cffb094e6c49e9a95bd5b0b8
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-10-16 20:03:49 +0500

    final commit

---
+++ ViewComponents/ProductDesc.cs
@@ -0,0 +1,15 @@
+﻿using FlouristStudio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlouristStudio.ViewComponents
+{
+    public class ProductDesc : ViewComponent
+    {
+        
+        public IViewComponentResult Invoke(Product ProdcutObj)
+        {
+            ViewData["id"] = ProdcutObj.Id;
+            return View();
+        }
+    }
+}
